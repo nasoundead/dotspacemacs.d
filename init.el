c7b8161af4e732512ccf86a046e5957b83a2f757
nasoundead <nasoundead@163.com>
2016-10-23 22:16:44 +0800

    auto complet

--- init.el
+++ init.el
@@ -37,7 +37,12 @@ values."
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
      helm
-     (auto-completion: variables auto-completion-enable-sort-by-usage t)
+     (auto-completion :variables
+                      auto-completion-return-key-behavior 'complete
+                      auto-completion-tab-key-behavior 'cycle
+                      auto-completion-complete-with-key-sequence nil
+                      auto-completion-complete-with-key-sequence-delay 0.1
+                      auto-completion-private-snippets-directory nil)
      better-defaults
      emacs-lisp
      git
